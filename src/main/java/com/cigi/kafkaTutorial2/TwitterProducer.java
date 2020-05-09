f3cf0d8bec6ce1eccbdafa182f6651ba0f617214
Cihangir Ozmus <cozmus@gmail.com>
2020-05-09 14:09:06 +0300

    high throughput producer (at the expense of a bit of latency and CPU usage) is added with the following config:
     properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
     properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); //ms
     properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32KB batch size

--- src/main/java/com/cigi/kafkaTutorial2/TwitterProducer.java
+++ src/main/java/com/cigi/kafkaTutorial2/TwitterProducer.java
@@ -28,7 +28,7 @@ public class TwitterProducer {
     private final String consumerSecret = System.getenv("TWITTER_API_SECRET_KEY");
     private final String token = System.getenv("TWITTER_API_TOKEN");
     private final String secret = System.getenv("TWITTER_API_TOKEN_SECRET");
-    List<String> terms = Lists.newArrayList("java");
+    List<String> terms = Lists.newArrayList("bitcoin", "usa", "politics", "sport", "soccer");
 
 
     public TwitterProducer() {
@@ -123,6 +123,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
+        //high throughput producer (at the expense of a bit of latency and CPU usage)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20"); //ms
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024)); //32KB batch size
 
         //create the producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
