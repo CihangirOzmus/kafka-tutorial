fa9a9beddb62e17a0eec28f35aae865d05abd901
Cihangir Ozmus <cozmus@gmail.com>
2020-05-09 23:10:13 +0300

    kafka-basics module is refactored.

--- kafka-basics/src/main/java/kafkaTutorial1/ConsumerDemoGroups.java
+++ kafka-basics/src/main/java/kafkaTutorial1/ConsumerDemoGroups.java
@@ -15,10 +15,13 @@ import java.util.Properties;
 public class ConsumerDemoGroups {
     public static void main(String[] args) {
         Logger logger = LoggerFactory.getLogger(ConsumerDemoGroups.class);
+        String bootstrapServers = "127.0.0.1:9092";
         String groupId = "my-fifth-application";
+        String topic = "first_topic";
+
         //create consumer configs
         Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
@@ -28,14 +31,14 @@ public class ConsumerDemoGroups {
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
 
         //subscribe consumer to our topic(s)
-        consumer.subscribe(Collections.singleton("first_topic"));
-//        consumer.subscribe(Arrays.asList("first_topic", "second_topic"));
+        consumer.subscribe(Collections.singleton(topic));
+        //consumer.subscribe(Arrays.asList("first_topic", "second_topic"));
 
         //poll for new data
-        while (true){
+        while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
 
-            for (ConsumerRecord<String, String> record: records) {
+            for (ConsumerRecord<String, String> record : records) {
                 logger.info("Key: " + record.key() + " Value: " + record.value());
                 logger.info("Partition: " + record.partition() + " Offset: " + record.offset());
             }
